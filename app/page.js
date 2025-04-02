3c2b2fa444cdf2d4d6efcd40473fc9c609815783
Kevin-Gao602 <Kevin_602@163.com>
2025-04-02 15:29:49 +1100

    Update pictures to page.js

--- app/page.js
+++ app/page.js
@@ -6,3 +6,20 @@ export default function Home() {
     </div>
   );
 }
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to StrataEase_management system</h1>
+      <p>This is a web application to help property management.</p>
+      <Image
+        src="/building.jpg"
+        alt="Building Image"
+        width={500} 
+        height={300} 
+        priority 
+      />
+    </div>
+  );
+}
