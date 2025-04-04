364ac9da1b8925b5ccda0426454084c2bb288c80
Kevin-Gao602 <Kevin_602@163.com>
2025-04-04 12:28:16 +1100

    add picture

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-以下是调整后的 `page.js` 代码，设置图片宽度为 `300px` ，高度为 `200px` ，你可以直接替换原文件内容使用：
-
-```jsx
 import Image from 'next/image';
 
 export default function Home() {
@@ -17,6 +14,3 @@ export default function Home() {
     </div>
   );
 }
-```
-
-记得通过 `npm run dev` 或 `yarn dev` 启动Next.js开发服务器来查看效果，而不要直接用 `node` 运行该文件。 
